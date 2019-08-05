a9f483a260699ca4d8ade56c6555399482433b0f
PavelSolovev <pashius2010@gmail.com>
2019-08-05 22:13:52 +0400

    first commit

---
+++ Chapter01/hello.ts
@@ -0,0 +1,11 @@
+// install Node.js https://nodejs.org/
+// install compiler:                      npm install -g typescript
+// view version compiler:                 tsc -v
+// compilation files from .ts to .js:     tsc hello.ts
+
+// add tsconfig.json:                     tsc -init
+// compilation using tsconfig.json:       tsc
+// localization console messages:         tsc --locale ru
+// add tasks.json:                        tsc --init
+
+console.log('Hello TypeScript');
