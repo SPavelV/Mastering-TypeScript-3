f2360c6d676d86f7117f9691319503bde8f1ee31
Solovev Pavel <p.solovev@elewise.com>
2019-09-05 12:28:17 +0400

    function

--- Chapter02/samples-functions.js
+++ Chapter02/samples-functions.js
@@ -1,14 +1,110 @@
 "use strict";
 // Functions
+// =========
 function addNumbers(a, b) {
     return (a + b).toString();
 }
 var addResult = addNumbers(2, 3);
 console.log("addNumbes returned : " + addResult);
-// Anonimus functons: 
+// Anonimus functons:
+// ==================
 var addVar = function (a, b) {
     return a + b;
 };
 var addVarResult = addVar(2, 3);
 console.log("addVarResult:", addVarResult);
+// Optional parameters:
+// ====================
+var concatStrings = function (a, b, c) {
+    return a + b + c;
+};
+var concat3strings = concatStrings("a", "b", "c");
+console.log("concat3strings : " + concat3strings);
+var concat2strings = concatStrings("a", "b");
+console.log("concat2strings : " + concat2strings);
+// const concat1string = concatStrings("a");
+// Default parameters
+// ==================
+function concatStringsDefault(a, b, c) {
+    if (c === void 0) { c = "c"; }
+    return a + b + c;
+}
+var defaultConcat = concatStringsDefault("a", "b");
+console.log("--- defaultConcat = " + defaultConcat);
+// rest parameters
+// ===============
+// function testArguments() {
+//   if(arguments.length > 0) {
+//     for(let i = 0; i < arguments.length; i++) {
+//       console.log("--- arguments[" + i + "] = " + arguments[i]);
+//     }
+//   }
+// }
+// testArguments(1, 2, 3);
+// testArguments("firstArg");
+function testArgumentsTS() {
+    var argArray = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        argArray[_i] = arguments[_i];
+    }
+    if (argArray.length > 0) {
+        for (var i_1 = 0; i_1 < argArray.length; i_1++) {
+            console.log("--- argArray[" + i_1 + "] = " + argArray[i_1]);
+            // use JavaScript arguments variable
+            console.log("--- arguments[" + i_1 + "] = " + arguments[i_1]);
+        }
+    }
+}
+testArgumentsTS(9);
+testArgumentsTS(1, 2, 3);
+function testNormalAndRestArguments(arg1, arg2) {
+    var argArray = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        argArray[_i - 2] = arguments[_i];
+    }
+}
+// Function callbacks
+// ==================
+// js implementation
+// const callbackFunction = function(text) {
+//   console.log("--- insideFunction", text);
+// };
+// function doSomethingWhithCallback(initialText: string, callback) {
+//   console.log("--- inside doSomthingWithCallback", initialText);
+//   if(typeof callback === "function") {
+//     callback(initialText);
+//   } else {
+//     console.log(initialText + " is not a function!");
+//   }
+// }
+// doSomethingWhithCallback("text", callbackFunction);
+// ts implementation
+function callbackFunction(text) {
+    console.log("--- inside callbackFunction = " + text);
+}
+function doSomethingWithACallback(initialText, callback) {
+    console.log("--- inside doSomethingWithCallback = " + initialText);
+    callback(initialText);
+}
+doSomethingWithACallback("myText", callbackFunction);
+function add(a, b) {
+    return a + b;
+}
+console.log("--- add(1,1) = " + add(1, 1));
+console.log("--- add(\"1\", \"1\") = " + add("1", "1"));
+// console.log(`--- add(true, false) = ${add(true, false)}`); 
+// error TS2345: Argument of type 'true' is not assignable to
+// parameter of type 'number'.
+// try catch
+// =========
+try {
+    console.log("--- 1. attempting to parse JSON");
+    JSON.parse("abcd=234");
+}
+catch (error) {
+    console.log("2. try catch error : " + error);
+}
+finally {
+    console.log("3. finaly");
+}
 //# sourceMappingURL=samples-functions.js.map
--- Chapter02/samples-functions.ts
+++ Chapter02/samples-functions.ts
@@ -1,4 +1,5 @@
 // Functions
+// =========
 
 function addNumbers(a: number, b: number) : string {
   return (a + b).toString();
@@ -8,7 +9,8 @@ const addResult = addNumbers(2, 3);
 console.log(`addNumbes returned : ${addResult}`);
 
 
-// Anonimus functons: 
+// Anonimus functons:
+// ==================
 
 const addVar = function(a:number, b:number) : number {
   return a + b;
@@ -19,6 +21,7 @@ console.log("addVarResult:", addVarResult);
 
 
 // Optional parameters:
+// ====================
 
 const concatStrings = function(a: string, b: string, c?: string) {
   return a + b + c;
@@ -30,4 +33,124 @@ console.log(`concat3strings : ${concat3strings}`);
 const concat2strings = concatStrings("a", "b");
 console.log(`concat2strings : ${concat2strings}`);
 
-const concat1string = concatStrings("a");
+// const concat1string = concatStrings("a");
+
+// Default parameters
+// ==================
+
+function concatStringsDefault(
+  a: string,
+  b: string,
+  c: string = "c") {
+    return a + b + c;
+}
+
+const defaultConcat = concatStringsDefault("a", "b");
+console.log(`--- defaultConcat = ${defaultConcat}`);
+
+// rest parameters
+// ===============
+
+// function testArguments() {
+//   if(arguments.length > 0) {
+//     for(let i = 0; i < arguments.length; i++) {
+//       console.log("--- arguments[" + i + "] = " + arguments[i]);
+//     }
+//   }
+// }
+// testArguments(1, 2, 3);
+// testArguments("firstArg");
+
+function testArgumentsTS(... argArray: number[]) {
+  if(argArray.length > 0) {
+    for(let i = 0; i < argArray.length; i++) {
+      console.log(`--- argArray[${i}] = ${argArray[i]}`);
+      // use JavaScript arguments variable
+      console.log(`--- arguments[${i}] = ${arguments[i]}`);
+    }
+  }
+}
+
+testArgumentsTS(9);
+testArgumentsTS(1, 2, 3);
+
+function testNormalAndRestArguments(
+  arg1: string,
+  arg2: number,
+  ...argArray: number[]
+) {
+
+}
+
+// Function callbacks
+// ==================
+
+// js implementation
+// const callbackFunction = function(text) {
+//   console.log("--- insideFunction", text);
+// };
+
+// function doSomethingWhithCallback(initialText: string, callback) {
+//   console.log("--- inside doSomthingWithCallback", initialText);
+//   if(typeof callback === "function") {
+//     callback(initialText);
+//   } else {
+//     console.log(initialText + " is not a function!");
+//   }
+// }
+// doSomethingWhithCallback("text", callbackFunction);
+
+// ts implementation
+function callbackFunction(text: string) {
+  console.log(`--- inside callbackFunction = ${text}`);
+}
+
+function doSomethingWithACallback(
+  initialText: string,
+  callback : (initialText: string) => void
+) {
+  console.log(`--- inside doSomethingWithCallback = ${initialText}`);
+  callback(initialText);
+}
+
+doSomethingWithACallback("myText", callbackFunction);
+
+// doSomethingWithACallback("myText", "this is not a function"); 
+// Argument of type '"this is not a function"' is not assignable 
+// to parameter of type '(initialText: string)=> void'.
+
+// Function overrides
+// ==================
+
+// js implementation
+// function add(x, y) {
+//   return x + y;
+// }
+// console.log("--- add(1, 1)", add(1,1));
+// console.log("--- add(\"1\",\"1\")=", add("1","1") );
+
+// ts implementation
+function add(a: string, b: string) : string;
+function add(a: number, b: number) : number;
+function add(a: any, b: any): any {
+  return a + b;
+}
+
+console.log(`--- add(1,1) = ${add(1,1)}`);
+console.log(`--- add("1", "1") = ${add("1", "1")}`);
+
+// console.log(`--- add(true, false) = ${add(true, false)}`); 
+// error TS2345: Argument of type 'true' is not assignable to
+// parameter of type 'number'.
+
+// try catch
+// =========
+
+try {
+  console.log("--- 1. attempting to parse JSON");
+  JSON.parse(`abcd=234`);
+} catch (error) {
+  console.log(`2. try catch error : ${error}`);
+} finally {
+  console.log(`3. finaly`);
+}
