0f16739678fc6d11a390ae70767004de9317a32b
PavelSolovev <pashius2010@gmail.com>
2019-10-16 22:38:52 +0400

    implementing interfaces

--- Chapter03/samples-classes.ts
+++ Chapter03/samples-classes.ts
@@ -8,3 +8,28 @@ class SimpleClass {
 let mySimpleClass = new SimpleClass();
 mySimpleClass.id = 1001;
 mySimpleClass.print();
+
+// Implementing Interfaces
+// ==========
+
+interface IPrint {
+  print() : void;
+}
+
+function printClass( a: IPrint) {
+  a.print();
+}
+
+class ClassA implements IPrint {
+  print() {console.log('ClassA.print()')};
+}
+
+class ClassB implements IPrint {
+  print() {console.log('ClassB.print()')};
+}
+
+let classA = new ClassA();
+let classB = new ClassB();
+
+printClass(classA);
+printClass(classB);
