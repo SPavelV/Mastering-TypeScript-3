fc17f8e6e37d188471efdff54b2100296da99dad
PavelSolovev <pashius2010@gmail.com>
2020-04-02 22:18:47 +0400

    Add method decorators

--- Chapter04/samples-decorators.js
+++ Chapter04/samples-decorators.js
@@ -1,6 +1,6 @@
 "use strict";
-// Decorator syntax
-// ================
+// // Decorator syntax
+// // ================
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -10,91 +10,108 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-function simpleDecorator(constructor) {
-    console.log('simpleDecorator called.');
-}
-var ClassWithSimpleDecorator = /** @class */ (function () {
-    function ClassWithSimpleDecorator() {
-    }
-    ClassWithSimpleDecorator = __decorate([
-        simpleDecorator
-    ], ClassWithSimpleDecorator);
-    return ClassWithSimpleDecorator;
-}());
-var instance_1 = new ClassWithSimpleDecorator();
-var instance_2 = new ClassWithSimpleDecorator();
-console.log("instance_1 : " + JSON.stringify(instance_1));
-console.log("instance_2 : " + JSON.stringify(instance_2));
-function secondDecorator(constructor) {
-    console.log('secondDecorator called.');
-}
-var ClassWithMultipleDecorators = /** @class */ (function () {
-    function ClassWithMultipleDecorators() {
-    }
-    ClassWithMultipleDecorators = __decorate([
-        simpleDecorator,
-        secondDecorator
-    ], ClassWithMultipleDecorators);
-    return ClassWithMultipleDecorators;
-}());
-// Decorators factories
-// ====================
-function decoratorFactory(name) {
-    return function (constructor) {
-        console.log("decorator function called with: " + name);
+// function simpleDecorator(constructor: Function) {
+//   console.log("simpleDecorator called.");
+// }
+// @simpleDecorator
+// class ClassWithSimpleDecorator {}
+// let instance_1 = new ClassWithSimpleDecorator();
+// let instance_2 = new ClassWithSimpleDecorator();
+// console.log(`instance_1 : ${JSON.stringify(instance_1)}`);
+// console.log(`instance_2 : ${JSON.stringify(instance_2)}`);
+// function secondDecorator(constructor: Function) {
+//   console.log("secondDecorator called.");
+// }
+// @simpleDecorator
+// @secondDecorator
+// class ClassWithMultipleDecorators {}
+// // Decorators factories
+// // ====================
+// function decoratorFactory(name: string) {
+//   return function(constructor: Function) {
+//     console.log(`decorator function called with: ${name}`);
+//   };
+// }
+// @decoratorFactory("testName")
+// class ClassWithDecoratorFactory {}
+// // Class decorator parameters
+// // ==========================
+// function classConstructorDec(constructor: Function) {
+//   console.log(`constructor : ${constructor}`);
+//   console.log(`constructor.name : ${<any>constructor.name}`);
+//   constructor.prototype.testProperty = "testProperty_value";
+// }
+// @classConstructorDec
+// class CalssWithConstructor {}
+// let classConstructorInstance = new CalssWithConstructor();
+// console.log(
+//   `classConstrInstance.testProperty : ${
+//     (<any>classConstructorInstance).testProperty
+//   }`
+// );
+// // Property decorators
+// // ===================
+// function propertyDec(target: any, propertyKey: string) {
+//   if (typeof target === "function") {
+//     console.log("class name:", target.name);
+//   } else {
+//     console.log("class name: ", target.constructor.name);
+//   }
+//   console.log("propertyKey: ", propertyKey);
+// }
+// class ClassWithPropertyDec {
+//   @propertyDec
+//   name: string | undefined;
+// }
+// class StaticClassWithPropertyDec {
+//   @propertyDec
+//   static aname: string;
+// }
+// Method decorators
+// =================
+// function methodDec(
+//   target: any,
+//   methodName: string,
+//   descriptor?: PropertyDescriptor
+// ) {
+//   console.log(`target: ${target}`);
+//   console.log(`methodName : ${methodName}`);
+//   console.log(`target[methodName] : ${target[methodName]}`);
+// }
+// class ClassWithMethodDec {
+//   @methodDec
+//   print(output: string) {
+//     console.log(`ClassWithMethodDec.print` + `(${output}) called.`);
+//   }
+// }
+// Using method decorators
+// =======================
+function auditLogDec(target, methodName, descriptor) {
+    var originalFunction = target[methodName];
+    var auditFunction = function () {
+        console.log("auditLogDec : overide of " + methodName + " called");
+        for (var index = 0; index < arguments.length; index++) {
+            var element = arguments[index];
+            console.log("arg : " + index + " = " + arguments[index]);
+        }
+        originalFunction.apply(this, arguments);
     };
+    target[methodName] = auditFunction;
+    return target;
 }
-var ClassWithDecoratorFactory = /** @class */ (function () {
-    function ClassWithDecoratorFactory() {
+var ClassWithAuditDec = /** @class */ (function () {
+    function ClassWithAuditDec() {
     }
-    ClassWithDecoratorFactory = __decorate([
-        decoratorFactory('testName')
-    ], ClassWithDecoratorFactory);
-    return ClassWithDecoratorFactory;
-}());
-// Class decorator parameters
-// ==========================
-function classConstructorDec(constructor) {
-    console.log("constructor : " + constructor);
-    console.log("constructor.name : " + constructor.name);
-    constructor.prototype.testProperty = "testProperty_value";
-}
-var CalssWithConstructor = /** @class */ (function () {
-    function CalssWithConstructor() {
-    }
-    CalssWithConstructor = __decorate([
-        classConstructorDec
-    ], CalssWithConstructor);
-    return CalssWithConstructor;
-}());
-var classConstructorInstance = new CalssWithConstructor();
-console.log("classConstrInstance.testProperty : " + classConstructorInstance.testProperty);
-// Property decorators
-// ===================
-function propertyDec(target, propertyKey) {
-    if (typeof (target) === 'function') {
-        console.log('class name: ', target.name);
-    }
-    else {
-        console.log('class name: ', target.constructor.name);
-    }
-    console.log('propertyKey: ', propertyKey);
-}
-var ClassWithPropertyDec = /** @class */ (function () {
-    function ClassWithPropertyDec() {
-    }
+    ClassWithAuditDec.prototype.print = function (arg1, arg2) {
+        console.log("ClassWithMethodDec.print " + arg1 + ", " + arg2 + " called.");
+    };
     __decorate([
-        propertyDec,
-        __metadata("design:type", Object)
-    ], ClassWithPropertyDec.prototype, "name", void 0);
-    return ClassWithPropertyDec;
+        auditLogDec,
+        __metadata("design:type", Function),
+        __metadata("design:paramtypes", [String, String]),
+        __metadata("design:returntype", void 0)
+    ], ClassWithAuditDec.prototype, "print", null);
+    return ClassWithAuditDec;
 }());
-var StaticClassWithPropertyDec = /** @class */ (function () {
-    function StaticClassWithPropertyDec() {
-    }
-    __decorate([
-        propertyDec,
-        __metadata("design:type", String)
-    ], StaticClassWithPropertyDec, "aname", void 0);
-    return StaticClassWithPropertyDec;
-}());
+var auditClass = new ClassWithAuditDec();
+auditClass.print("test1", "test2");
--- Chapter04/samples-decorators.ts
+++ Chapter04/samples-decorators.ts
@@ -2,7 +2,7 @@
 // ================
 
 function simpleDecorator(constructor: Function) {
-  console.log('simpleDecorator called.');
+  console.log("simpleDecorator called.");
 }
 
 @simpleDecorator
@@ -15,7 +15,7 @@ console.log(`instance_1 : ${JSON.stringify(instance_1)}`);
 console.log(`instance_2 : ${JSON.stringify(instance_2)}`);
 
 function secondDecorator(constructor: Function) {
-  console.log('secondDecorator called.');
+  console.log("secondDecorator called.");
 }
 
 @simpleDecorator
@@ -28,18 +28,18 @@ class ClassWithMultipleDecorators {}
 function decoratorFactory(name: string) {
   return function(constructor: Function) {
     console.log(`decorator function called with: ${name}`);
-  }
+  };
 }
 
-@decoratorFactory('testName')
-class ClassWithDecoratorFactory{}
+@decoratorFactory("testName")
+class ClassWithDecoratorFactory {}
 
 // Class decorator parameters
 // ==========================
 
 function classConstructorDec(constructor: Function) {
   console.log(`constructor : ${constructor}`);
-  console.log(`constructor.name : ${(<any>constructor.name)}`);
+  console.log(`constructor.name : ${<any>constructor.name}`);
   constructor.prototype.testProperty = "testProperty_value";
 }
 
@@ -47,19 +47,23 @@ function classConstructorDec(constructor: Function) {
 class CalssWithConstructor {}
 
 let classConstructorInstance = new CalssWithConstructor();
-console.log(`classConstrInstance.testProperty : ${(<any>classConstructorInstance).testProperty}`);
+console.log(
+  `classConstrInstance.testProperty : ${
+    (<any>classConstructorInstance).testProperty
+  }`
+);
 
 // Property decorators
 // ===================
 
 function propertyDec(target: any, propertyKey: string) {
-  if(typeof(target) === 'function') {
-    console.log('class name: ', target.name);
+  if (typeof target === "function") {
+    console.log("class name:", target.name);
   } else {
-    console.log('class name: ', target.constructor.name);
+    console.log("class name: ", target.constructor.name);
   }
 
-  console.log('propertyKey: ', propertyKey )
+  console.log("propertyKey: ", propertyKey);
 }
 
 class ClassWithPropertyDec {
@@ -71,3 +75,55 @@ class StaticClassWithPropertyDec {
   @propertyDec
   static aname: string;
 }
+
+// Method decorators
+// =================
+
+function methodDec(
+  target: any,
+  methodName: string,
+  descriptor?: PropertyDescriptor
+) {
+  console.log(`target: ${target}`);
+  console.log(`methodName : ${methodName}`);
+  console.log(`target[methodName] : ${target[methodName]}`);
+}
+
+class ClassWithMethodDec {
+  @methodDec
+  print(output: string) {
+    console.log(`ClassWithMethodDec.print` + `(${output}) called.`);
+  }
+}
+
+// Using method decorators
+// =======================
+
+function auditLogDec(
+  target: any,
+  methodName: string,
+  descriptor?: PropertyDescriptor
+) {
+  let originalFunction = target[methodName];
+  let auditFunction = function(this: any) {
+    console.log(`auditLogDec : overide of ${methodName} called`);
+    for (let index = 0; index < arguments.length; index++) {
+      const element = arguments[index];
+      console.log(`arg : ${index} = ${arguments[index]}`);
+    }
+    originalFunction.apply(this, arguments);
+  };
+
+  target[methodName] = auditFunction;
+  return target;
+}
+
+class ClassWithAuditDec {
+  @auditLogDec
+  print(arg1: string, arg2: string) {
+    console.log(`ClassWithMethodDec.print ${arg1}, ${arg2} called.`);
+  }
+}
+
+let auditClass = new ClassWithAuditDec();
+auditClass.print("test1", "test2");
