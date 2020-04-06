98543dc536de0ccb58e1287dd819ff21c016b865
PavelSolovev <pashius2010@gmail.com>
2020-04-06 23:17:50 +0400

    generics

--- Chapter04/samples-generics.ts
+++ Chapter04/samples-generics.ts
@@ -1,14 +1,26 @@
 // Generics
 // ========
 
-class Concatenator<T> {
-  concatenateArray(inputArray: Array<T>): string {
+class Concatenator< T > {
+  concatenateArray(inputArray: Array< T >): string {
     let returnString = "";
     for (let index = 0; index < inputArray.length; index++) {
       if (index > 0) returnString += ",";
-      returnString += inputArray[index].toString();
+      returnString += [index].toString();
     }
 
     return returnString;
   }
 }
+
+const stringConcat = new Concatenator<string>();
+const numberConcat = new Concatenator<number>();
+
+const stringArray: string[] = ["first", "second", "third"];
+const numberArray: number[] = [1, 2, 3];
+
+const stringResult = stringConcat.concatenateArray(stringArray);
+const numberResult = numberConcat.concatenateArray(numberArray);
+
+const stringResult2 = stringConcat.concatenateArray(numberArray);
+const numberResult2 = numberConcat.concatenateArray(stringArray);
